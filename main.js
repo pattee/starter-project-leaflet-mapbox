a270ec6796650b17bb059e11a88e06d3e0f30bb7
pattee <pattee13@gmail.com>
2017-05-27 12:11:47 -0400

    starter commit

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
-  /*
-   * Application Variables
-   */
-  // Access token
-  L.mapbox.accessToken = 'pk.eyJ1IjoicGF0dGVlMTMiLCJhIjoiU01MUEZQRSJ9.GOzX8bvCA3k1xZ46RiYymA';
+/*
+ * Application Variables
+ */
+// Access token
+L.mapbox.accessToken = 'pk.abunchoflettersandnuberyougetfrommapboxaccountaccesstoken';
 
 
-  /*
-   * Application Init
-   */
-  var map = L.mapbox.map('map', 'pattee13.in2og2la', {
-    center: [35.59, -82.56], // lat, long
-    zoom: 13
-  });
+/*
+ * Application Init
+ */
+var mymap = L.mapbox.map('map', 'username.createaprojectandputidhere', {
+  center: [51.505, -0.09],
+  zoom: 13
+});
 
-  L.mapbox.tileLayer('mapbox.pencil', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18
-  }).addTo(map);
+L.mapbox.tileLayer('mapbox.streets', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(mymap);
+
+var oneplace = L.marker([51.505, -0.09]).addTo(mymap);
+oneplace.bindPopup("<b>Place one</b><br>I am a popup.").openPopup();
+
+var twoplace = L.marker([51.5, -0.09]).addTo(mymap);
+twoplace.bindPopup("<b>Place two</b><br>I a place with a popup");
+
+var circle = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(mymap);
+circle.bindPopup("I am a circle.");
+
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap);
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+//provides a popup when you click
+function onMapClick(e) {
+  popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
